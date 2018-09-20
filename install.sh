d391c975c03b8b2767166fa6ae55e345a038ab23
jackal <pep8@pm.me>
2018-09-20 15:35:32 +0200

    support for perl scripts in install

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ cp $install_folder/vim/.vimrc $HOME/
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 pip install flake8
 echo COPYING OLD-HOPE COLOR TO VIM CONFIG
-cp -r $install_folder/vim/autoload $HOME/.vim/
-cp -r $install_folder/vim/colors $HOME./vim/
+cp -rf $install_folder/vim/autoload $HOME/.vim/
+cp -rf $install_folder/vim/colors $HOME./vim/
 vim +PluginInstall +qall
 youcompleteme_install=$HOME/.vim/bundle/YouCompleteMe/install.py
 python $youcompleteme_install
@@ -26,6 +26,7 @@ cp -r $install_folder/openbox $HOME/.config/
 # urxvt
 echo COPYING URXVT CONFIG FILE TO $HOME/.Xdefaults
 cp -r $install_folder/urxvt/.Xdefaults $HOME/
+mkdir -p $HOME/.urxvt/etx && cp $install_folder/urxvt/scripts/* $HOME/.urxvt/etx
 
 # bash
 cp $install_folder/bash/.bashrc $HOME/
