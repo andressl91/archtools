425b36d739719bd6ff3e429def31ad5e46f41f95
jackal <pep8@pm.me>
2019-06-17 20:49:45 +0200

    Updated intsall.sh with functions

--- install.sh
+++ install.sh
@@ -1,64 +1,82 @@
 #!/bin/bash
 
-
 install_folder=$(dirname $(readlink -f $0)) 
 
-####### Vim #######
-sudo pacman -S vim vim-runtime
-echo COPYING VIM CONFIG TO $HOME/.vimrc
-cp $install_folder/vim/.vimrc $HOME/
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-pip install flake8
-echo COPYING OLD-HOPE COLOR TO VIM CONFIG
-cp -rf $install_folder/vim/autoload $HOME/.vim/
-cp -rf $install_folder/vim/colors $HOME/.vim/
-vim +PluginInstall +qall
-youcompleteme_install=$HOME/.vim/bundle/YouCompleteMe/install.py
-# IF NOT C++ needed install with  python $youcompleteme_install
-python $youcompleteme_install --clang-completer
-sudo pacman -S python ipython python-virtualenv
-
-# WINDOW MANAGER i3-wm
-echo COPY i3 CONFIG TO HOME DIRECTORY
-cp $install_folder/i3wm/config $HOME/.config/i3
-
-####### TERMINAL ######
-# urxvt
-sudo pacman -S rxvt-unicode
-echo COPYING URXVT CONFIG FILE TO $HOME/.Xdefaults
-cp -r $install_folder/urxvt/.Xdefaults $HOME/
-mkdir -p $HOME/.urxvt/etx && cp $install_folder/urxvt/scripts/* $HOME/.urxvt/etx
-
-# bash
-echo COPYING BASHRC CONFIG FILE
-cp $install_folder/bash/.bashrc $HOME/
-
 ######## SYSTEM #########
-sudo pacman -S htop openssh
-
+sudo pacman -S openssh htop
 # Make ssh-key
 echo GENERATE SSH-KEY
 ssh-keygen -t rsa -b 4096
+sudo pacman -S python ipython python-virtualenv 
 
-# Internet browser
+# COPY XORG CONFIG FILE TO HOMEFOLDER
+cp ${install_folder}/xorg/.xinitrc $HOME
+
+####### Vim #######
+function install_vim {
+    sudo pacman -S vim vim-runtime
+    echo COPYING VIM CONFIG TO $HOME/.vimrc
+    cp $install_folder/vim/.vimrc $HOME/
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    pip install flake8
+    echo COPYING OLD-HOPE COLOR TO VIM CONFIG
+    cp -rf $install_folder/vim/autoload $HOME/.vim/
+    cp -rf $install_folder/vim/colors $HOME/.vim/
+    vim +PluginInstall +qall
+    youcompleteme_install=$HOME/.vim/bundle/YouCompleteMe/install.py
+    # IF NOT C++ needed install with  python $youcompleteme_install
+    python $youcompleteme_install --clang-completer
+}
+
+# WINDOW MANAGER i3-wm
+function install_i3wm {
+    echo COPY i3 CONFIG TO HOME DIRECTORY
+    cp $install_folder/i3wm/config $HOME/.config/i3
+}
+
+###### RICE ######
+function install_rice {
+    echo RICE UP THE DESKTOP
+    sudo pacman -S feh imagemagick python-pip python-pywal
+    mkdir -p $HOME/Pictures/Wallpaper
+    cp $install_folder/rice/leopard.png $HOME/Pictures/Wallpaper
+    cp $install_folder/rice/leopard.jpg $HOME/Pictures/Wallpaper
+    echo NOW RUN: wal -i "PATH TO IMAGE", and watch the magic
+}
+
+####### TERMINAL urxvt ######
+function install_urxvt {
+    sudo pacman -S rxvt-unicode
+    echo COPYING URXVT CONFIG FILE TO $HOME/.Xdefaults
+    cp -r $install_folder/urxvt/.Xdefaults $HOME/
+    mkdir -p $HOME/.urxvt/etx && cp $install_folder/urxvt/scripts/* $HOME/.urxvt/etx
+}
+
+# bash
+function install_bash_config {
+    echo COPYING BASHRC CONFIG FILE
+    cp $install_folder/bash/.bashrc $HOME/
+}
+
+
+######## INSTALL FIREFOX #######3
 sudo pacman -S firefox
 
 ######## PRIVACY #########
 # VPN and macchanger
-sudo pacman -S openresolv openvn macchanger
-sudo cp ${install_folder}/openvpn/update-resolv-conf /etc/openvpn/
-echo ADD FOLLOWING IN OPENVPN client.conf
-echo script-security 2
-echo up /etc/openvpn/update-resolv-conf.sh
-echo down /etc/openvpn/update-resolv-conf.sh
-echo down-pre
+function install_privacy_tools {
+    sudo pacman -S openresolv openvpn macchanger
+    sudo cp ${install_folder}/openvpn/update-resolv-conf /etc/openvpn/
+    echo ADD FOLLOWING IN OPENVPN client.conf
+    echo script-security 2
+    echo up /etc/openvpn/update-resolv-conf.sh
+    echo down /etc/openvpn/update-resolv-conf.sh
+    echo down-pre
+}
 
-###### RICE ######
-echo RICE UP THE DESKTOP
-sudo pacman -S feh imagemagick python-pip python-pywal
-mkdir -p $HOME/Pictures/Wallpaper
-cp $install_folder/rice/leopard.png $HOME/Pictures/Wallpaper
-cp $install_folder/rice/leopard.jpg $HOME/Pictures/Wallpaper
-echo NOW RUN: wal -i "PATH TO IMAGE", and watch the magic
-
-
+install_vim()
+install_i3wm()
+install_rice()
+install_urxvt()
+install_bash_config()
+install_privacy_tools()
