e782c01ede2d22919a95daa8268080234c92c7c7
jackal <pep8@pm.me>
2019-08-19 09:13:35 +0200

    crosscompiler scripts cleanuo

--- crosscompile/chinx.sh
+++ crosscompile/chinx.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-CROSS_COMPILE_ROOT=/home/jackal/arm64cross/aarch64-unknown-linux-gnu
-CROSS_COMPILE_BIN=${CROSS_COMPILE_ROOT}/bin
+#CHINX_SCRIPT=$(readlink -f $0)
+#echo ${SCRIPT}
+UTILITIES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export CROSS_ENVIRONMENT_ROOT="$(cd ${UTILITIES_DIR}/../ && pwd)"
+CROSS_COMPILE_BIN=${CROSS_ENVIRONMENT_ROOT}/bin
 
-TARGET_ROOT=/home/jackal/arm64cross/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu
-ARM_PACMAN_CONFIG=${TARGET_ROOT}/etc/pacman.conf
-TOOLCHAIN_CMAKE_FILE=/path/to/cross_arm64.cmake
 
+export ARCHITECTURE="aarch64-unknown-linux-gnu"
+export TARGET_ROOT=${CROSS_ENVIRONMENT_ROOT}/${ARCHITECTURE}
 
 export PATH=${CROSS_COMPILE_BIN}:$PATH
+echo $PATH
+
 function pacman {
+    ARM_PACMAN_CONFIG=${TARGET_ROOT}/etc/pacman.conf
     command pacman --config=${ARM_PACMAN_CONFIG} $*
 }
 
+function spacman {
+    ARM_PACMAN_CONFIG=${TARGET_ROOT}/etc/pacman.conf
+    command sudo pacman --config=${ARM_PACMAN_CONFIG} $*
+}
+
 function cmake {
-    command cmake -DCMAKE_TOOLCHAIN_FILE=${CROSS_COMPILE_ROOT}/utilities/cross_arm64.cmake $*
+    TOOLCHAIN_CMAKE_FILE=${CROSS_ENVIRONMENT_ROOT}/utilities/cross_arm64.cmake
+    command cmake -DCMAKE_TOOLCHAIN_FILE=${UTILITIES_DIR}/cross_arm64.cmake $*
 }
--- crosscompile/cross_arm64.cmake
+++ crosscompile/cross_arm64.cmake
@@ -1,54 +1,76 @@
 ###### Link Compile Path ##############
-set(CROSS_SYS_ROOT_PATH /home/jackal/arm64cross/aarch64-unknown-linux-gnu) 
-set(ARM_SYS_ROOT_PATH /home/jackal/arm64cross/aarch64-unknown-linux-gnu/aarch64-unknown-linux-gnu) 
 
-set(ARM_STD_USR_PATH ${ARM_SYS_ROOT_PATH}/usr)
-set(ARM_STD_LIB_PATH ${ARM_SYS_ROOT_PATH}/usr/lib)
-set(ARM_SYS_LIB_PATH ${ARM_SYS_ROOT_PATH}/lib)
-set(ARM_STD_INC_PATH ${ARM_SYS_ROOT_PATH}/usr/include)
+# TESTING: ASSUMES FOLLOWING VARIABLES FROM chinx.sh is defined
+#set(CROSS_SYS_ROOT_PATH ${CROSS_ENVIRON_ROOT}) 
+#set(ARM_SYS_ROOT_PATH ${TARGET_ROOT}) 
 
+set(TARGET_STD_USR_PATH ${TARGET_ROOT}/usr)
+set(TARGET_STD_LIB_PATH ${TARGET_ROOT}/usr/lib)
+set(TARGET_SYS_LIB_PATH ${TARGET_ROOT}/lib)
+set(TARGET_STD_INC_PATH ${TARGET_ROOT}/usr/include)
 
-set(GCC_MACHINE_DUMP aarch64-unknown-linux-gnu)
-set(CMAKE_SYSTEM_NAME aarch64-unknown-linux-gnu)
+set(ARCHITECTURE $ENV{ARCHITECTURE})
+set(CROSS_ENVIRONMENT_ROOT $ENV{CROSS_ENVIRONMENT_ROOT})
+#set(GCC_MACHINE_DUMP ${ARCHITECTURE})
+set(CMAKE_SYSTEM_NAME ${ARCHITECTURE})
 
 # THIS THING IS DANGEROUS, READ UP#
-#set(CMAKE_SYSROOT ${ARM_SYS_ROOT_PATH})
-##searchs in X/lib and X/usr/lib
-set(CMAKE_FIND_ROOT_PATH ${ARM_SYS_ROOT_PATH} ${CROSS_SYS_ROOT_PATH}) 
+#set(CMAKE_SYSROOT ${TARGET_ROOT})
+#By default the CMAKE_FIND_ROOT_PATH is empty.
+#set(CMAKE_FIND_ROOT_PATH ${TARGET_ROOT} ${CROSS_ENVIRONMENT_ROOT}) 
 
-set(CMAKE_INCLUDE_PATH  ${ARM_STD_INC_PATH} ; ${ARM_STD_INC_PATH}/${GCC_MACHINE_DUMP} )
-set(CMAKE_LIBRARY_PATH  ${ARM_SYS_LIB_PATH} ; ${ARM_STD_LIB_PATH}/${GCC_MACHINE_DUMP} ; ${ARM_STD_LIB_PATH} ; ${ARM_SYS_LIB_PATH}/${GCC_MACHINE_DUMP})
-set(CMAKE_PROGRAM_PATH  ${ARM_SYS_ROOT_PATH}/usr/bin})
+# MIGHT NOT NEED THIS, CMAKE_PREFIX_PATH might be enough
+message( " TARGET ROOT IS is $ENV{TARGET_ROOT}" )
+set(CMAKE_FIND_ROOT_PATH $ENV{TARGET_ROOT}) 
+message( "CMAKE_FIND_ROOT_PATH is ${CMAKE_FIND_ROOT_PATH}" )
 
-# AVOID DYNAMIC LIBRARY TEST FOR LINKER, WILL FAIL IF NOT.
-# CMAKE_FORCE_"LANGUAGE"_COMPILER WILL OVERRUN, BUT UGLY
+# CMAKE_PREFIX_PATH search a specified dir for lib/, /include/, bin/
+# list of paths where the following cmake functions will be used
+# - find_package()
+# - find_program()
+# - find_library()
+# - find_file()
+# - find_path()
+
+                  ##### IMPORTANT ####  
+# CMAKE_PREFIX_PATX ony search /lib, /include, NOT usr/lib usr/include
+set(CMAKE_PREFIX_PATH $ENV{TARGET_ROOT}/usr) # add more locations with semiconlon separated list
+
+# Here one can add further paths to search for include,lib speficif folders separatly 
+#set(CMAKE_INCLUDE_PATH  ${TARGET_STD_INC_PATH} ; ${TARGET_STD_INC_PATH}/${GCC_MACHINE_DUMP} )
+#set(CMAKE_LIBRARY_PATH  ${TARGET_SYS_LIB_PATH} ; ${TARGET_STD_LIB_PATH}/${GCC_MACHINE_DUMP} ; ${TARGET_STD_LIB_PATH} ; ${TARGET_SYS_LIB_PATH}/${GCC_MACHINE_DUMP})
+#set(CMAKE_PROGRAM_PATH  ${TARGET_ROOT}/usr/bin})
+
+# WARNING!
+# DURING CONFIGURATION, CMAKE WILL TEST THE SELECTED COMPILER.
+# DURING CROSSCOMPILING, THESE TEST WILL FAIL(THE LINKER) DUE TOO TRYNIC TO RUN
+# THE DYNAMIC LINKER. 
+# TO OPTIONS:
+# -  FORCE STATIC LIBRARY INSTEAD.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
-
-#set(CMAKE_LIBRARY_PATH "${ARM_SYS_ROOT_PATH}/lib")
+# - JUMP OVER TRY_COMPILE USING THE CMAKE_FORCE_LANGUAGE (UGLY BUT WORKS)
 
 ##### CROSS-COMPILER BINARIES
-set(CROSS_ARM_BIN      "${CROSS_SYS_ROOT_PATH}/bin") 
-set(CMAKE_C_COMPILER   "${CROSS_ARM_BIN}/${CMAKE_SYSTEM_NAME}-gcc")
-set(CMAKE_CXX_COMPILER "${CROSS_ARM_BIN}/${CMAKE_SYSTEM_NAME}-g++")
-set(CMAKE_AR           "${CROSS_ARM_BIN}/${CMAKE_SYSTEM_NAME}-ar")
-set(CMAKE_RANLIB       "${CROSS_ARM_BIN}/${CMAKE_SYSTEM_NAME}-ranlib")
-set(CMAKE_LINKER       "${CROSS_ARM_BIN}/${CMAKE_SYSTEM_NAME}-ld")
+set(CROSS_ARM_BIN      "${CROSS_ENVIRONMENT_ROOT}/bin") 
+set(CMAKE_C_COMPILER   "${CROSS_ARM_BIN}/${ARCHITECTURE}-gcc")
+set(CMAKE_CXX_COMPILER "${CROSS_ARM_BIN}/${ARCHITECTURE}-g++")
+set(CMAKE_AR           "${CROSS_ARM_BIN}/${ARCHITECTURE}-ar")
+set(CMAKE_RANLIB       "${CROSS_ARM_BIN}/${ARCHITECTURE}-ranlib")
+set(CMAKE_LINKER       "${CROSS_ARM_BIN}/${ARCHITECTURE}-ld")
 
 ########### Macro to Limit Find path in host and targert ###################
-
-
-## REMEMBER, IF WE REQUIRE LIBS/INCLUDE FROM OTHER LOCATIONS
-# COMMENT OUT CMAKE_FIND_ROOT_PATH_MODE_****, ELSE IT CAN'T FIND..
-
-
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+option(TARGET_ROOT_ONLY "Search only for lib, include, package in CMAKE_FIND_ROOT_DIR" ON)
 # for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+if (TARGET_ROOT_ONLY)
+    set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+    set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+    set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+endif(TARGET_ROOT_ONLY)
 
 
 
-#set(CMAKE_INSTALL_PREFIX ${ARM_SYS_ROOT_PATH}/usr/local/OpenCV ) ## install Path
+#set(CMAKE_INSTALL_PREFIX ${TARGET_ROOT}/usr/local/OpenCV ) ## install Path
 
